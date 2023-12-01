033c0d93549165410a1922f6a3f2e906361bd85c
Mrestof <fotserm@gmail.com>
2023-12-01 22:11:30 +0200

    2023:1:1: remove debug stuff

--- 2023/1/main.rs
+++ 2023/1/main.rs
@@ -2,6 +2,9 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 
+// TODO: expect unwraps
+
+
 fn get_num(line: String) -> u32 {
     let mut left_char = '0';
     let mut right_char = '0';
@@ -11,18 +14,15 @@ fn get_num(line: String) -> u32 {
             break;
         }
     }
-    println!("left char: {left_char}");
     for char in line.chars().rev() {
         if char.is_digit(10) {
             right_char = char;
             break;
         }
     }
-    println!("right char: {right_char}");
     let left_digit: u32 = left_char.to_digit(10).unwrap();
     let right_digit: u32 = right_char.to_digit(10).unwrap();
     let res: u32 = left_digit * 10 + right_digit;
-    println!("res: {res}");
     return res
 }
 
@@ -33,12 +33,11 @@ fn main() {
     let reader = BufReader::new(file);
     let mut sum: u32 = 0;
     // Read the file line by line
-    for (index, line) in reader.lines().enumerate() {
+    for line in reader.lines() {
         let line = line.unwrap();  // ignore errors
         // show the line and its number
         println!("{}", line);
         sum += get_num(line);
-        // if index == 3 { break };
     }
     println!("first answer: {sum}");
 }
