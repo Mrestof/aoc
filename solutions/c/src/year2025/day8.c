ffddc8dedbe4fa081f3b68627db789b22c8d5c33
Mrestof <mrestof@disroot.org>
2025-12-08 16:46:46 +0100

    c:25:8:1 add a broken solution (need to fix later)

--- solutions/c/src/year2025/day8.c
+++ solutions/c/src/year2025/day8.c
@@ -1,5 +1,6 @@
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "myutils.h"
 
@@ -11,8 +12,133 @@ static char input[MAXINPUT];
 static char *lines[MAXLINES];
 static int nlines;
 
-static ll part1() {
+typedef struct JunctionBox {
+  Point pos;
+  struct JunctionBox *adj[MAXLINES];
+} JunctionBox;
+
+typedef struct {
+  JunctionBox *b1, *b2;
+  double distance;
+} Pair;
+
+static Pair pairs[MAXLINES*MAXLINES];
+static int npairs = 0;
+static JunctionBox jboxes[MAXLINES] = {0};
+static int njboxes = 0;
+
+static int int_cmp(const void *a, const void *b) {
+  int n1 = *(const int *)a;
+  int n2 = *(const int *)b;
+  if (n1 < n2) return -1;
+  if (n1 > n2) return 1;
   return 0;
+}
+
+static int pair_cmp(const void *a, const void *b) {
+  Pair p1 = *(const Pair *)a;
+  Pair p2 = *(const Pair *)b;
+  if (p1.distance < p2.distance) return -1;
+  if (p1.distance > p2.distance) return 1;
+  return 0;
+}
+
+static void get_boxes() {
+  int i;
+  JunctionBox *b;
+  for (i = 0; i < nlines; i++) {
+    b = &jboxes[i];
+    sscanf(lines[i], "%lf,%lf,%lf", &b->pos.x, &b->pos.y, &b->pos.z);
+    njboxes++;
+  }
+}
+
+static void get_pairs() {
+  int i, j;
+  Pair *pair;
+  for (i = 0; i < njboxes; i++) {
+    for (j = i+1; j < njboxes; j++) {
+      pair = &pairs[npairs++];
+      pair->b1 = &jboxes[i];
+      pair->b2 = &jboxes[j];
+      pair->distance = distance(&pair->b1->pos, &pair->b2->pos);
+    }
+  }
+}
+
+static bool in_circuit(JunctionBox *prev, JunctionBox *b1, JunctionBox *b2) {
+  for (int i = 0; b1->adj[i] != NULL; i++) {
+    if (b1->adj[i] == prev)
+      continue;
+    if (b1->adj[i] == b2)
+      return true;
+    if (in_circuit(b1, b1->adj[i], b2))
+      return true;
+  }
+  return false;
+}
+
+static void add_adj(JunctionBox *target, JunctionBox *new) {
+  int i;
+  for (i = 0; target->adj[i] != NULL; i++);
+  target->adj[i] = new;
+}
+
+static void connect_pair(Pair p) {
+  if (!in_circuit(NULL, p.b1, p.b2)) {
+    add_adj(p.b1, p.b2);
+    add_adj(p.b2, p.b1);
+  }
+}
+
+static size_t get_jbox_idx(JunctionBox *addr) {
+  JunctionBox *base = jboxes;
+  return addr - base;
+}
+
+static int count_boxes_in_circuit(
+  JunctionBox *prev,
+  JunctionBox *b,
+  bool seen[MAXLINES]
+) {
+  int sum = 1;
+  seen[get_jbox_idx(b)] = true;
+  for (int i = 0; b->adj[i] != NULL; i++) {
+    if (b->adj[i] == prev)
+      continue;
+    sum += count_boxes_in_circuit(b, b->adj[i], seen);
+  }
+  return sum;
+}
+
+static int eval_circuits() {
+  int i, ncl;
+  int res;
+  bool seen[MAXLINES] = {0};
+  int ciruit_length[MAXLINES];
+  for (i = 0; i < MAXLINES; i++) {
+    if (seen[i]) continue;
+    ciruit_length[ncl++] = count_boxes_in_circuit(NULL, &jboxes[i], seen);
+  }
+  qsort(ciruit_length, ncl, sizeof(int), int_cmp);
+  res = 1;
+  for (i = 0; i < 3; i++)
+    res *= ciruit_length[ncl-i-1];
+  return res;
+}
+
+static ll part1() {
+  int i;
+
+  get_boxes();
+  get_pairs();
+
+  qsort(pairs, nlines-1, sizeof(Pair), pair_cmp);
+
+  for (i = 0; i < npairs; i++)
+    connect_pair(pairs[i]);
+
+  return eval_circuits();
 }
 
 static ll part2() {
